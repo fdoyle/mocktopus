c974d1aa8ae7d5789394f3c441c5d25afe5c3fe7
Frank Doyle <frank.doyle@willowtreeapps.com>
2014-07-22 15:14:27 -0400

    added StringWebpageUrl annotation

---
+++ app/src/main/java/com/lacronicus/mocktopus/mocktopusdriver/mocktopus/annotation/StringWebpageUrl.java
@@ -0,0 +1,16 @@
+package com.lacronicus.mocktopus.mocktopusdriver.mocktopus.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by fdoyle on 7/22/14.
+ */
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface StringWebpageUrl {
+    String value() default "http://www.google.com"; //todo change this
+}
+
